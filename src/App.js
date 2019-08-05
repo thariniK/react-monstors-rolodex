fea7df332d34396a88070d4d9ee5aa8bdb5c67c0
Tharini <tharini.kandasamy@gympik.com>
2019-08-06 01:30:43 +0530

    Monstors Rolodex: Final Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      string: 'Hello World!!!'
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ''
+        }
     }
-  }
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {this.state.string}
-            </p>
-            <button onClick={() => this.setState({string: 'Changed!!!'})}>Change Text</button>
-          </header>
-        </div>
-    );
-  }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({monsters: users}));
+    }
+
+    handleChange = (e) => { //binds automatically aka lexical scoping
+        this.setState({searchField: e.target.value})
+    };
+
+    render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter((monster) => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+        return (
+            <div className="App">
+                <h1>Monster Rolodex</h1>
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 export default App;
