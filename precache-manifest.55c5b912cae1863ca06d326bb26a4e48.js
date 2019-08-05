66db8c172db642fcfbc0f7625965ea0a3fe19e8d
Tharini <tharini.kandasamy@gympik.com>
2019-08-06 01:46:03 +0530

    Updates

---
+++ precache-manifest.55c5b912cae1863ca06d326bb26a4e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76ec468747abf1d0bb607299b8986ca",
+    "url": "/react-monstors-rolodex/index.html"
+  },
+  {
+    "revision": "6928372413db96b969e2",
+    "url": "/react-monstors-rolodex/static/css/main.eba0f2f9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monstors-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6928372413db96b969e2",
+    "url": "/react-monstors-rolodex/static/js/main.0456ad96.chunk.js"
+  },
+  {
+    "revision": "25a7583055516da3aa72",
+    "url": "/react-monstors-rolodex/static/js/runtime~main.a4af0a15.js"
+  }
+]);
